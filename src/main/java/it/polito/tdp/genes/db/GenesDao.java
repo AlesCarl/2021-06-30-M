f50b69a62c78ea54471160393093572131a789f7
gianmarcocarlone <gianmarcocarlone@localhost>
2023-06-16 17:05:06 +0200

    piu o meno

--- src/main/java/it/polito/tdp/genes/db/GenesDao.java
+++ src/main/java/it/polito/tdp/genes/db/GenesDao.java
@@ -14,13 +14,19 @@ import it.polito.tdp.genes.model.Interactions;
 
 public class GenesDao {
 	
-	public List<Genes> getAllGenes(){
-		String sql = "SELECT DISTINCT GeneID, Essential, Chromosome FROM Genes";
+	
+	
+	public List<Genes> getGenesCromosoma(int cromosoma){
+		String sql = "select g.* "
+				+ "from genes g "
+				+ "where g.`Chromosome`= ? " ;
+		
 		List<Genes> result = new ArrayList<Genes>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, cromosoma);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
 
@@ -39,7 +45,183 @@ public class GenesDao {
 		}
 	}
 	
+	
+	
+	//solo per popolare la mappa 
+	public void getAllGenes(Map<String,Genes> idMap){
+		String sql = "select g.* "
+				+ "from genes g ";
+		
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				Genes genes = new Genes(res.getString("GeneID"), 
+						res.getString("Essential"), 
+						res.getInt("Chromosome"));
+				
+				idMap.put(res.getString("GeneID"), genes); 
+				
+			}
+			res.close();
+			st.close();
+			conn.close();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("Database error", e) ;
+		}
+	}
+	
+	
+	
+	//vertici grafo
+	public List<Integer> getAllCromosomi(){
+		String sql = " select distinct g.`Chromosome` "
+				+ "from genes g "
+				+ "where g.`Chromosome`!= 0 "; 
+		
+		List<Integer> result = new ArrayList<Integer>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			
+			while (res.next()) {
+				result.add(res.getInt("g.Chromosome"));
+				
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("Database error", e) ;
+		}
+	}
+	
+	
+	//NON CREDO SERVA
+	public List<Interactions> getAllInteractions( Map<String,Genes> idMap  ){
+		
+		String sql = "select i.`GeneID1`, i.`GeneID2`,i.`Type`, i.`Expression_Corr` "
+				+ "from interactions i "; 
+
+		
+		List<Interactions> result = new ArrayList<Interactions>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			
+			while (res.next()) {
+				
+				Interactions inter = new Interactions (idMap.get(res.getString("i.GeneID1")), idMap.get(res.getString("i.GeneID2")),res.getString("i.Type"),
+						res.getDouble("i.Expression_Corr"));
+				
+				result.add(inter);
+				
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("Database error", e) ;
+		}
+	}
 
 
+
+	public boolean collegamentoArchi(int c1, int c2) {
+		
+
+		String sql = "select distinct i.*\n"
+				+ "from interactions i, genes g1, genes g2 "
+				+ "where i.`GeneID1`= g1.`GeneID` and i.`GeneID2`= g2.`GeneID` "
+				+ "and g1.`Chromosome`= ? and g2.`Chromosome`=? "; 
+		
+				//+ "and g1.`GeneID`='G234334' and g2.`GeneID`='G234684' "; 
+
+		
+		boolean result= false; 
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, c1);
+			st.setInt(2, c2);
+
+			ResultSet res = st.executeQuery();
+			
+		
+				
+			//res.first();
+			while (res.next()) {
+			
+			if(res.getString("i.GeneID1")!= null && res.getString("i.GeneID2")!= null) 
+				result= true;  
+			//vuol dire che esiste un arco. 
+				
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("Database error", e) ;
+		}
+	}
+
+
+
+	public double getSumCorrelazione(Integer c1, Integer c2) {
+		
+		
+		String sql = "select g1.`Chromosome`,  g2.`Chromosome`,  sum(distinct i.`Expression_Corr`) as cn "
+				+ "from interactions i, genes g1, genes g2 "
+				+ "where i.`GeneID1`= g1.`GeneID` and i.`GeneID2`= g2.`GeneID`  "
+				+ "and g1.`Chromosome`= ? and g2.`Chromosome`= ? " ; 
+		
+				// 5 , 11
+
+		
+		double sum= 0.0; 
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, c1);
+			st.setInt(2, c2);
+
+			ResultSet res = st.executeQuery();
+			
+		
+				
+			res.first();
+			sum = res.getDouble("cn"); 
+			//vuol dire che esiste un arco. 
+				
+				
+			res.close();
+			st.close();
+			conn.close();
+			return sum;
+			
+		} catch (SQLException e) {
+			throw new RuntimeException("Database error", e) ;
+		}
+	}
+
+
+	
 	
 }
--- src/main/java/it/polito/tdp/genes/model/Model.java
+++ src/main/java/it/polito/tdp/genes/model/Model.java
@@ -1,5 +1,127 @@
 package it.polito.tdp.genes.model;
 
-public class Model {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.genes.db.GenesDao;
+
+
+public class Model {
+	
+	
+	GenesDao dao; 
+    private SimpleDirectedWeightedGraph<Integer, DefaultWeightedEdge> graph;  // SEMPLICE, PESATO, NON ORIENTATO
+    private List<Integer> allCromosomi ; 
+    
+    Map <String,Genes>idMapGenes= new HashMap<>();
+    
+
+    
+
+    public Model() {
+    	
+    	this.dao= new GenesDao();  
+    	this.graph= new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
+    	this.allCromosomi= new ArrayList<>();
+    	
+    }
+    
+    private void loadAllNodes() {
+		
+    	allCromosomi= dao.getAllCromosomi(); 
+    	dao.getAllGenes(idMapGenes); 
+ 				
+ 		}
+    
+    
+    public void creaGrafo() {
+		 
+		 
+		 loadAllNodes(); 
+	 		//System.out.println("size: " +this.allNACode.size());
+
+		 /** VERTICI */
+	    	Graphs.addAllVertices(this.graph, allCromosomi);
+	 		System.out.println("NUMERO vertici GRAFO: " +this.graph.vertexSet().size());
+	 		
+	 		
+	 		/** ARCHI */
+	 		
+	 		/*
+	 		Un arco collega due CROMOSOMI diversi solo se: 
+	 		
+	 		i due cromosomi contengono due geni (uno per cromosoma) che compaiono 
+	 		(nello stesso ordine) nella tabella interactions. 
+	 		
+	 		Si noti che, per ciascun cromosoma, possono esistere più geni, e 
+	 		ciascuno di essi potrebbe essere presente 
+	 		più volte (associato a function diverse).
+	 		*/
+	 		
+	 		
+	 		
+	 		/*
+	 1. dato un cromosoma, trovo tutti i geni che hanno quel cromosoma --> listGenesDaCromosoma
+	 2. 
+	 		 */
+	 		
+	 		for(Integer c1: this.allCromosomi) {
+	 			for(Integer c2: this.allCromosomi) {
+	 				
+	 				if(c1!=c2) {  // provo così perche orientato
+	 					double peso= calcoloPeso(c1,c2); 
+	 			 
+	 			 				if( peso>0 ) {
+	 			 					
+	 		 						Graphs.addEdgeWithVertices(this.graph, c1, c2, peso);
+	 		 						
+	 		        }
+	 			}
+	 		}
+   	 }
+	 		System.out.println("\nNUMERO ARCHI GRAFO: " +this.graph.edgeSet().size());
+	
+	 }
+
+    //arco c1 - c2
+	private double calcoloPeso(Integer c1, Integer c2) {
+		
+		double peso = 0.0; 
+		
+		List<Genes> listGen1= dao.getGenesCromosoma(c1); //all geni del cromosoma c1
+		List<Genes> listGen2= dao.getGenesCromosoma(c2); //all geni del cromosoma c2
+		
+		
+		
+		for(Genes g1: listGen1 ) {
+			for(Genes g2: listGen2 ) {
+				if(g1.getGeneId().compareTo(g2.getGeneId())!= 0) {
+					
+					if(dao.collegamentoArchi(g1.getChromosome(),g2.getChromosome())== true) { // i due geni sono nella stessa function
+						return dao.getSumCorrelazione(c1,c2); //calcolo il peso 
+					} 
+					else if(dao.collegamentoArchi(g2.getChromosome(),g1.getChromosome())== true) { // i due geni sono nella stessa function
+						return dao.getSumCorrelazione(c2,c1);
+					
+				}
+			}
+			
+		}
+		
+		
+	}
+    
+		return peso;
+	
+	
+	}
+	
+    
+    
 }
